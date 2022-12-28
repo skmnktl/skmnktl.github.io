711be07d801dff94dd34e792960bf5f35c66e9a4
śrīnivāsa kaśyap munukutla <skmnktl@gmail.com>
2022-12-27 20:56:28 -0600

    cleanups

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,5 +6,14 @@ export default defineNuxtConfig({
     css: [
         '@fortawesome/fontawesome-svg-core/styles.css',
         '~/assets/fonts/alegreya.css'
-    ]
+    ],
+    link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Alegreya+SC:ital,wght@0,400;0,500;0,700;0,800;1,400;1,500;1,700;1,800&family=Alegreya+Sans+SC:ital,wght@0,100;0,300;0,400;0,500;0,700;0,800;1,100;1,300;1,400;1,500;1,700;1,800&family=Alegreya+Sans:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700&family=Alegreya:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap'
+        }
+    ],
+    script: [{
+        src: 'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
+    }]
 })
